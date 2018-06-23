89e7838e6f395581d527aa51e3b587b9cb8fd388
unknown <zhemeaisunday@gmail.com>
2018-06-23 22:01:37 +0800

    format

--- p40lx210.cpp
+++ p40lx210.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 string global_string;
 int global_int;
-int main() {
-  int local_int;
-  string local_string;
-  cout << global_string << endl;
-  cout << global_int << endl;
-  cout << local_string << endl;
-  cout << local_int << endl;
-  return 0;
+int main()
+{
+    int local_int;
+    string local_string;
+    cout << global_string << endl;
+    cout << global_int << endl;
+    cout << local_string << endl;
+    cout << local_int << endl;
+    return 0;
 }
