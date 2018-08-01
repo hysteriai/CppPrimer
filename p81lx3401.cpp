5ff2f8f156d0b283d55df0950ca05a615577a50e
Unknown <zhemeaisunday@gmail.com>
2018-08-01 23:05:16 +0800

    modify format

--- p81lx3401.cpp
+++ p81lx3401.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-  string s1, s2;
-  cin >> s1 >> s2;
-  if (s1 == s2) {
-    cout << "s1==s2" << endl;
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1 == s2)
+    {
+        cout << "s1==s2" << endl;
+        return 0;
+    }
+    cout << "s1!=s2" << endl;
     return 0;
-  }
-  cout << "s1!=s2" << endl;
-  return 0;
 }
