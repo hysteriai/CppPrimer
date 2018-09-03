1b586fe896313d2cbdbf5bc9ab0688e4a895a3d5
Hysteria <zhemeaisunday@gmail.com>
2018-09-03 23:28:36 +0800

    提交练习题

---
+++ p140lx428.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << sizeof(int) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(long long int) << endl;
+    return 0;
+}
